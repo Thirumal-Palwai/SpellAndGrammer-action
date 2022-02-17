8bb3775efbd3769dfe13101b37b29e6925917c0c
Palwai <thirumalreddy.palwai@philips.com>
2022-02-17 20:52:06 +0530

    new word adition

--- checks.sh
+++ checks.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 set -e
 
-if ! exists vale; then
-  echo 'Installing vale'
-  curl -sfL https://install.goreleaser.com/github.com/ValeLint/vale.sh | sh -s v2.9.1
-  export PATH="./bin:$PATH"
-fi
+echo 'Installing vale'
+curl -sfL https://install.goreleaser.com/github.com/ValeLint/vale.sh | sh -s v2.9.1
+export PATH="./bin:$PATH"
 
 vale .
